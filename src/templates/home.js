3bace14dbac414921dffe7343471644213112c71
mae1884 <miaia@altamontenterprise.com>
2021-04-07 16:45:10 -0500

    commit on 'master'

--- src/templates/home.js
+++ src/templates/home.js
@@ -15,17 +15,6 @@ export const query = graphql`
     }
   }
 `;
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
-<script type="text/javascript">
-  algoliasearchNetlify({
-    appId: '786H6PUISD',
-    apiKey: '774ad11ba7693af7a6649a3a59da3cbb',
-    siteId: 'ac70d6f3-147d-43c9-b798-31f06967fa46',
-    branch: 'master',
-    selector: 'div#search',
-  });
-</script>
 export default class Home extends React.Component {
     render() {
         let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/posts'), 'frontmatter.date', 'desc');
